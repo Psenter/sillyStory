cf24724dd59ad62746cb2ebbd1478b4993410533
Parker S <parker.senter@gmail.com>
2023-06-01 13:51:08 +0000

    Fixed HTML, scrapped JS and recompleted steps 1-3.

--- main.js
+++ main.js
@@ -2,31 +2,39 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
     let newStory = storyText;
-    xItem = randomValueFromArray(insertX);
-    yItem = randomValueFromArray(insertY);
-    zItem = randomValueFromArray(insertZ);
-    newStory = newStory.replace(':insertX:', xItem);
-    newStory = newStory.replace(':insertY:', yItem);
-    newStory = newStory.replace(':insertY:', yItem);
-    newStory = newStory.replace(':insertZ:', zItem);
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    
+  if(customName.value !== '') {
+    const name = customName.value;
 
-    if (customName.value !== '') {
-        newStory = newStory.replace('Bob', name);
-    }
-    if (document.getElementById("uk").checked) {
-        const weight = Math.round(300);
-        const temperature = Math.round(94);
-    }
+  }
 
-    story.textContent = ;
-    story.style.visibility = 'visible';
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
 }
